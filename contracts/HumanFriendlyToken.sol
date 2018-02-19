a0379e0a946aae7935413cff2b8533a273f7c367
Alan Lu <alanlu1023@gmail.com>
2018-02-19 13:20:50 +0100

    v0.1.0

---
+++ contracts/HumanFriendlyToken.sol
@@ -0,0 +1,16 @@
+/// Implements ERC 20 Token standard including extra accessors for human readability.
+/// See: https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md
+pragma solidity ^0.4.18;
+import "./Token.sol";
+
+
+/// @title Abstract human-friendly token contract - Functions to be implemented by token contracts
+contract HumanFriendlyToken is Token {
+
+    /*
+     *  Public functions
+     */
+    function name() public view returns (string);
+    function symbol() public view returns (string);
+    function decimals() public view returns (uint8);
+}
