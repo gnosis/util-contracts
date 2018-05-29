e532c2c2f955ab70d6258183f4a4aa93540be976
Angel Rodriguez <anxolin@gmail.com>
2018-05-29 12:16:04 +0200

    Fix warnings from contract compilation

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -4,24 +4,24 @@ contract Migrations {
     address public owner;
     uint public last_completed_migration;
 
-    modifier restricted() {
+    modifier restricted () {
         if (msg.sender == owner) _;
     }
 
-    function Migrations()
+    constructor ()
         public
     {
         owner = msg.sender;
     }
 
-    function setCompleted(uint completed)
+    function setCompleted (uint completed)
         public
         restricted
     {
         last_completed_migration = completed;
     }
 
-    function upgrade(address new_address)
+    function upgrade (address new_address)
         public
         restricted
     {
