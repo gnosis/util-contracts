d9eefb6feca0cd4ecf6e61780cfe06bae2ff787f
Angel Rodriguez <anxolin@gmail.com>
2018-05-29 12:32:10 +0200

    Deploy in an async funcion

--- migrations/2_deploy_basic_contracts.js
+++ migrations/2_deploy_basic_contracts.js
@@ -1,8 +1,15 @@
 const Math = artifacts.require('Math')
 const EtherToken = artifacts.require('EtherToken')
 
+async function deploy (deployer) {
+  // deploy Math
+  await (deployer.deploy(Math, { overwrite: false }))
+
+  // Deploy EtherToken
+  await deployer.link(Math, EtherToken)
+  await deployer.deploy(EtherToken, { overwrite: false })
+}
+
 module.exports = function (deployer) {
-  deployer.deploy(Math, { overwrite: false })
-  deployer.link(Math, EtherToken)
-  deployer.deploy(EtherToken, { overwrite: false })
+  return deploy(deployer)
 }
