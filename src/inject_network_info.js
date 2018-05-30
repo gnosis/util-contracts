f83aca90fcfd5cc8c50669bac8ebf6878860e87a
Angel Rodriguez <anxolin@gmail.com>
2018-05-30 14:59:06 +0200

    Define extractor/injector utils to refactor other projects

--- src/inject_network_info.js
+++ src/inject_network_info.js
@@ -1,16 +1,8 @@
-const networkUtils = require('./util/networkUtils')
-const DEFAULT_CONF_FILE = './conf/network-restore'
+const injectNetworks = require('./util/injectNetworks')
+const path = require('path')
 
-async function inject () {
-  const confFile = process.env.CONF_FILE || DEFAULT_CONF_FILE
-  console.log('Extract networks - Using conf file: %s', confFile)
-  const conf = require(confFile)
+const DEFAULT_CONF_FILE = path.join(__dirname, './conf/network-restore')
 
-  const { networksFile, buildDir } = conf
-
-  console.log(`Inject networks from ${networksFile} into built contracts`)
-  await networkUtils.updateBuiltContract(buildDir, networksFile)
-  console.log('Success! All networks were injected into the built contracts')
-}
-
-inject().catch(console.error)
+const confFile = process.env.CONF_FILE || DEFAULT_CONF_FILE
+injectNetworks(confFile)
+  .catch(console.error)
---
+++ src/util/injectNetworks.js
@@ -0,0 +1,18 @@
+const networkUtils = require('./networkUtils')
+const DEFAULT_CONF_FILE = './conf/network-restore'
+
+async function injectNetworks (configFilePath) {  
+  console.log('Extract networks - Using conf file: %s', configFilePath)
+  const conf = require(configFilePath)
+
+  const { networksFile, buildDir } = conf
+
+  console.log(`Inject networks from ${networksFile} into built contracts`)
+  await networkUtils.updateBuiltContract(buildDir, networksFile)
+  console.log('Success! All networks were injected into the built contracts')
+}
+
+
+module.exports = async function (configFilePath) {
+  return injectNetworks(configFilePath)
+}
