83e7c66beca07e5297d64c1a209eec8cfbf00447
Angel Rodriguez <anxolin@gmail.com>
2018-06-19 17:43:05 +0200

    Use the pending blocks also to get the nonce by default

--- src/util/HDWalletProvider.js
+++ src/util/HDWalletProvider.js
@@ -3,11 +3,12 @@ const Web3 = require('web3')
 console.log('Create HDWalletProvider')
 
 class HDWalletProvider extends TruffleHDWalletProvider {
-  constructor ({ mnemonic, url, blockForNonceCalculation = 'latest' }) {
+  constructor ({ mnemonic, url, blockForNonceCalculation = 'pending' }) { // latest
     // console.log('[HDWalletProvider] New provider for: %s', url)
     super(mnemonic, url)
     this._web3 = new Web3(this)
     this._address = this.addresses[0]
+    this._blockForNonceCalculation = blockForNonceCalculation
     // console.log('[HDWalletProvider] Wallet address: ', this._address)
   }
 
@@ -24,11 +25,12 @@ class HDWalletProvider extends TruffleHDWalletProvider {
     if (method === 'eth_sendTransaction') {
       const [, callback] = arguments
       // console.log('[HDWalletProvider] Send transaction params: ', options.params)
-      this._web3.eth.getTransactionCount(this._address, blockForNonceCalculation, (error, nonce) => {
+      this._web3.eth.getTransactionCount(this._address, this._blockForNonceCalculation, (error, nonce) => {
         if (error) {
           console.error('[HDWalletProvider] Error getting the nonce')
           callback(error)
         } else {
+          console.log('[HDWalletProvider] Using nonce: ', nonce)
           options.params.nonce = nonce
           // console.log('[HDWalletProvider] Params: ', params)
           return super.sendAsync(options, callback)
