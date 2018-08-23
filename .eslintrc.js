128131173a45ba6a5ca3913f1c072bd0e4af8bd1
Alan Lu <alanlu1023@gmail.com>
2018-08-23 17:36:23 -0500

    Set basis for 2.x

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,5 +8,5 @@ module.exports = {
   env: {
     'es6': true,
     'node': true
-  }
+  },
 }
--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,11 +1,3 @@
-/* global artifacts */
-/* eslint no-undef: "error" */
-
-const Migrations = artifacts.require('Migrations')
-
 module.exports = function (deployer) {
-  // FIXME: Truffle was not updating the migrations
-  // Strangely was fixed after manually adding the network config for
-  // compiled Migration contracts.
-  deployer.deploy(Migrations)
+  deployer.deploy(artifacts.require('Migrations'))
 }
