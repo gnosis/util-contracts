ea3ec2df6dd213195cadff924c7d0adc7bb5b3df
Angel Rodriguez <anxolin@gmail.com>
2018-12-03 16:39:50 +0100

    Add dotenv and example config

--- truffle.js
+++ truffle.js
@@ -4,6 +4,9 @@ const DEFAULT_GAS_PRICE_GWEI = 5
 const GAS_LIMIT = 5e6
 const DEFAULT_MNEMONIC = 'candy maple cake sugar pudding cream honey rich smooth crumble sweet treat'
 
+// Load env vars
+require('dotenv').config()
+
 // Get the mnemonic
 const privateKey = process.env.PK
 let mnemonic = process.env.MNEMONIC
@@ -15,8 +18,8 @@ if (!privateKey && !mnemonic) {
 const solcUseDocker = process.env.SOLC_USE_DOCKER === 'true' || false
 const solcVersion = '0.4.25'
 
+// Gas price
 const gasPriceGWei = process.env.GAS_PRICE_GWEI || DEFAULT_GAS_PRICE_GWEI
-const gas = GAS_LIMIT
 
 // Allow to add an aditional network (useful for docker-compose setups)
 //  i.e. NETWORK='{ "name": "docker", "networkId": "99999", "url": "http://rpc:8545", "gas": "6700000", "gasPrice": "25000000000"  }'
@@ -26,7 +29,7 @@ module.exports = truffleConfig({
   mnemonic,
   privateKey,
   gasPriceGWei,
-  gas,
+  gas: GAS_LIMIT,
   aditionalNetwork,
   solcUseDocker,
   solcVersion
