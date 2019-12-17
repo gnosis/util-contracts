ea5d12900a2d62ad5e02803393271e6cda0e0e70
josojo <josojo@hotmail.de>
2019-12-17 14:17:17 +0100

    new hdwallet

--- src/util/HDWalletProvider.js
+++ src/util/HDWalletProvider.js
@@ -1,54 +1,64 @@
-const TruffleHDWalletProvider = require('truffle-hdwallet-provider')
-const Web3 = require('web3')
+const TruffleHDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
 
 class HDWalletProvider extends TruffleHDWalletProvider {
-  constructor ({ mnemonic, privateKeys, url, blockForNonceCalculation = 'pending' }) { // latest
+  constructor({
+    mnemonic,
+    privateKeys,
+    url,
+    blockForNonceCalculation = "pending"
+  }) {
+    // latest
     // console.log('[HDWalletProvider] New provider for: %s', url)
-    const accountCredentials = privateKeys || mnemonic
-    super(accountCredentials, url)
-    this._web3 = new Web3(this)
-    this._address = this.addresses[0]
-    this._blockForNonceCalculation = blockForNonceCalculation
+    const accountCredentials = privateKeys || mnemonic;
+    super(accountCredentials, url);
+    this._web3 = new Web3(this);
+    this._address = this.addresses[0];
+    this._blockForNonceCalculation = blockForNonceCalculation;
     // console.log('[HDWalletProvider] Wallet address: ', this._address)
   }
 
-  sendAsync (params) {
-    let options
+  sendAsync(params) {
+    let options;
     if (Array.isArray(params)) {
-      options = params[0]
+      options = params[0];
     } else {
-      options = params
+      options = params;
     }
     // console.log('[HDWalletProvider] Intercepting sendAsync: ', arguments)
-    const { method } = options
+    const { method } = options;
     // console.log('[HDWalletProvider] Intercepting sendAsync - Method: %s', method)
-    if (method === 'eth_sendTransaction') {
-      const [, callback] = arguments
+    if (method === "eth_sendTransaction") {
+      const [, callback] = arguments;
       // console.log('[HDWalletProvider] Send transaction params: ', options.params)
-      this._web3.eth.getTransactionCount(this._address, this._blockForNonceCalculation, (error, nonce) => {
-        if (error) {
-          console.error('[HDWalletProvider] Error getting the nonce')
-          callback(error)
-        } else {
-          console.log('[HDWalletProvider] Using nonce: ', nonce)
-          options.params.nonce = nonce
-          // console.log('[HDWalletProvider] Params: ', params)
-          return super.sendAsync(options, callback)
+      this._web3.eth.getTransactionCount(
+        this._address,
+        this._blockForNonceCalculation,
+        (error, nonce) => {
+          if (error) {
+            console.error("[HDWalletProvider] Error getting the nonce");
+            callback(error);
+          } else {
+            console.log("[HDWalletProvider] Using nonce: ", nonce);
+            options.params.nonce = nonce;
+            // console.log('[HDWalletProvider] Params: ', params)
+            return super.sendAsync(options, callback);
+          }
         }
-      })
+      );
     } else {
-      return super.sendAsync(...arguments)
+      return super.sendAsync(...arguments);
     }
   }
 
-  _sendAsyncWithNonce () {
-    return super.sendAsync()
+  _sendAsyncWithNonce() {
+    return super.sendAsync();
   }
 
-  send () {
+  send() {
     // console.log('[HDWalletProvider] Intercepting send: ', arguments)
-    return super.send(...arguments)
+    return super.send(...arguments);
   }
 }
 
-module.exports = HDWalletProvider
+module.exports = HDWalletProvider;
