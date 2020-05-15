4638cba481e257e49eca56a8e008d802a6224e33
Felix Leupold <felixleupold90@gmail.com>
2020-05-15 17:32:30 +0200

    [Easy] linting (#29)
    This PR runs `yarn lint --fix` on the codebase and makes it so that we can start writing unit tests (exposing mocha and other globals in the eslint.rc)
    
    ### Test Plan
    
    Yarn lint no longer complains.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,19 @@ module.exports = {
   extends: 'standard',
   plugins: [],
   rules: {
-    'strict': 0,
-    'arrow-parens': [2, 'as-needed']
+    strict: 0,
+    'arrow-parens': [2, 'as-needed'],
+    'no-undef': "error",
   },
   env: {
-    'es6': true,
-    'node': true
-  }
+    es6: true,
+    node: true,
+    mocha: true,
+  },
+  globals: {
+    artifacts: false,
+    contract: false,
+    assert: false,
+    web3: false,
+  },
 }
--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,6 +1,3 @@
-/* global artifacts */
-/* eslint no-undef: "error" */
-
 const Migrations = artifacts.require('Migrations')
 
 module.exports = function (deployer) {
