a635776a000ec2d0f0aff0167bec901f0b4c09d0
Felix Leupold <felixleupold90@gmail.com>
2020-05-29 18:21:50 +0200

    [StorageReading] Base contract that exposes contract storage for external reading (#30)
    This PR introduces a new utility base contract which allows other smart contracts and offchain infrastructure to efficiently read internal storage (by inheriting from `StorageAccessible`).
    
    This is achieved by exposing a single `getStorageAt` method (similar to [the web3 method](https://web3js.readthedocs.io/en/v1.2.0/web3-eth.html?highlight=getStorageAt#getstorageat)), which takes as an argument the storage slot that should be read.
    
    There are 2 advantages of this method over using the web3 one.
    
    1. It also allows reading multiple words at once, making it more efficient to read larger structs or arrays of data with one roundtrip
    2. It can be invoked by other smart contracts (e.g. contracts integrating with Gnosis Protocol that want to read the private balance variable of a user)
    
    Given that all storage is already readable to the public via consecutive web3 calls there is no security concern of exposing it more conveniently.
    
    One could argue that smart contracts should not have any private fields in the first place. However, even with only public fields some nested information (such as the elements in an array on a top level struct) are not automatically exposed (and assuming to foresee all data fetching requirements at deployment time has proven flawed).
    
    In a follow up PR, we can also expose a more convenient way of reading internal storage, which would expose a method to do a "static delegate call" to another contract. That other contract can be controlled by the caller and use arbitrary solidity code to act on the local storage of the original contract.
    
    This will be similar to the [state override feature in geth](https://geth.ethereum.org/docs/rpc/ns-eth#3-object---state-override-set) but make it client agnostic and available to other smart contracts.
    
    However, the functionality here is a first step and seems valuable in itself since it doesn't require deploying a "viewer" smart contract and can thus prove cheaper for small data fetching use cases.
    
    ### Test Plan
    
    I added unit tests that use the `getStorageAt` method to query data from a wrapper smart contract that inherits from `StorageAccessible`.
    
    `yarn truffle test`

---
+++ contracts/test/StorageAccessibleWrapper.sol
@@ -0,0 +1,50 @@
+pragma solidity ^0.5.2;
+import "../StorageAccessible.sol";
+
+
+contract StorageAccessibleWrapper is StorageAccessible {
+    struct FooBar {
+        uint256 foo;
+        uint256 bar;
+    }
+
+    uint8 public constant SLOT_FOO = 0;
+    uint8 public constant SLOT_BAR = 1;
+    uint8 public constant SLOT_BAM = 1;
+    uint8 public constant SLOT_BAZ = 2;
+    uint8 public constant SLOT_QUX = 3;
+    uint8 public constant SLOT_FOOBAR = 4;
+
+    uint256 foo;
+    uint128 bar;
+    uint64 bam;
+    uint256[] baz;
+    mapping(uint256 => uint256) qux;
+    FooBar foobar;
+
+    constructor() public {}
+
+    function setFoo(uint256 foo_) public {
+        foo = foo_;
+    }
+
+    function setBar(uint128 bar_) public {
+        bar = bar_;
+    }
+
+    function setBam(uint64 bam_) public {
+        bam = bam_;
+    }
+
+    function setBaz(uint256[] memory baz_) public {
+        baz = baz_;
+    }
+
+    function setQuxKeyValue(uint256 key, uint256 value) public {
+        qux[key] = value;
+    }
+
+    function setFoobar(uint256 foo_, uint256 bar_) public {
+        foobar = FooBar({foo: foo_, bar: bar_});
+    }
+}
