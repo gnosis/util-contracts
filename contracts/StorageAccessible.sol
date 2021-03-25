bb5fe5fb5df6d8400998094fb1b32a178a47c3a1
Nicholas Rodrigues Lordello <nlordell@gmail.com>
2021-03-25 07:45:18 +0100

    StorageAccessible contract optimizations (#48)
    This PR changes the storage accessible implementation to use assembly in order to be more effecient, both in code costs as well as gas costs.
    
    ### Benchmarks
    
    Code size was reduced by 45% :tada:
    ```
    $ git checkout storage-accessible-optimizations; yarn compile && cat build/contracts/StorageAccessible.json | jq '.bytecode | length'
    ...
    1704
    $ git checkout solc-7; yarn compile && cat build/contracts/StorageAccessible.json | jq '.bytecode | length'   
    ...
    3094
    ```
    
    In order to check runtime execution costs, since we don't have any benchmarking in place, I just applied the following patch and ran the tests on both the base branch and this branch:
    ```diff
    diff --git a/test/storage_accessible.js b/test/storage_accessible.js
    index b67b73e..db4d034 100644
    --- a/test/storage_accessible.js
    +++ b/test/storage_accessible.js
    @@ -62,6 +62,7 @@ contract('StorageAccessible', () => {
           // Deploy and use reader contract to access foo
           const reader = await ExternalStorageReader.new()
           const getFooCall = reader.contract.methods.getFoo().encodeABI()
    +      console.log((await instance.simulateDelegatecall(reader.address, getFooCall)).receipt.gasUsed)
           const result = await instance.simulateDelegatecall.call(reader.address, getFooCall)
           assert.equal(42, fromHex(result))
         })
    ```
    Results:
    ```
    $ git checkout storage-accessible-optimizations && git stash apply; yarn test
    ...
    25910
    ...
    $ git checkout solc-7 && git stash apply; yarn test
    ...
    28110
    ...
    ```
    
    Discounting the base 21K transaction gas cost, thats a 30% reduction in delegate call execution of the `getFooCall` :tada:.
    
    ### Test Plan
    
    Exiting tests continue to pass without modification.

--- contracts/StorageAccessible.sol
+++ contracts/StorageAccessible.sol
@@ -52,50 +52,88 @@ contract StorageAccessible {
      */
     function simulateDelegatecall(
         address targetContract,
-        bytes memory calldataPayload
+        bytes calldata calldataPayload
     ) public returns (bytes memory response) {
-        bytes memory innerCall = abi.encodeWithSelector(
-            this.simulateDelegatecallInternal.selector,
-            targetContract,
-            calldataPayload
-        );
-        (, response) = address(this).call(innerCall);
-        bool innerSuccess = response[response.length - 1] == 0x01;
-        setLength(response, response.length - 1);
-        if (innerSuccess) {
-            return response;
-        } else {
-            revertWith(response);
+        assembly {
+            let internalCalldata := mload(0x40)
+            // Store `simulateDelegatecallInternal.selector`.
+            mstore(internalCalldata, "\x43\x21\x8e\x19")
+            // Abuse the fact that both this and the internal methods have the
+            // same signature, and differ only in symbol name (and therefore,
+            // selector) and copy calldata directly. This saves us approximately
+            // 250 bytes of code and 300 gas at runtime over the
+            // `abi.encodeWithSelector` builtin.
+            calldatacopy(
+                add(internalCalldata, 0x04),
+                0x04,
+                sub(calldatasize(), 0x04)
+            )
+
+            // `pop` is required here by the compiler, as top level expressions
+            // can't have return values in inline assembly. `call` typically
+            // returns a 0 or 1 value indicated whether or not it reverted, but
+            // since we know it will always revert, we can safely ignore it.
+            pop(call(
+                gas(),
+                address(),
+                0,
+                internalCalldata,
+                calldatasize(),
+                // The `simulateDelegatecallInternal` call always reverts, and
+                // instead encodes whether or not it was successful in the return
+                // data. The first 32-byte word of the return data contains the
+                // `success` value, so write it to memory address 0x00 (which is
+                // reserved Solidity scratch space and OK to use).
+                0x00,
+                0x20
+            ))
+
+
+            // Allocate and copy the response bytes, making sure to increment
+            // the free memory pointer accordingly (in case this method is
+            // called as an internal function). The remaining `returndata[0x20:]`
+            // contains the ABI encoded response bytes, so we can just write it
+            // as is to memory.
+            let responseSize := sub(returndatasize(), 0x20)
+            response := mload(0x40)
+            mstore(0x40, add(response, responseSize))
+            returndatacopy(response, 0x20, responseSize)
+
+            if iszero(mload(0x00)) {
+                revert(add(response, 0x20), mload(response))
+            }
         }
     }
 
     /**
      * @dev Performs a delegetecall on a targetContract in the context of self.
-     * Internally reverts execution to avoid side effects (making it static). Returns encoded result as revert message
-     * concatenated with the success flag of the inner call as a last byte.
+     * Internally reverts execution to avoid side effects (making it static).
+     *
+     * This method reverts with data equal to `abi.encode(bool(success), bytes(response))`.
+     * Specifically, the `returndata` after a call to this method will be:
+     * `success:bool || response.length:uint256 || response:bytes`.
+     *
      * @param targetContract Address of the contract containing the code to execute.
      * @param calldataPayload Calldata that should be sent to the target contract (encoded method name and arguments).
      */
     function simulateDelegatecallInternal(
         address targetContract,
         bytes memory calldataPayload
-    ) external returns (bytes memory response) {
-        bool success;
-        (success, response) = targetContract.delegatecall(
-            calldataPayload
-        );
-        revertWith(abi.encodePacked(response, success));
-    }
-
-    function revertWith(bytes memory response) internal pure {
+    ) external {
         assembly {
-            revert(add(response, 0x20), mload(response))
-        }
-    }
+            let success := delegatecall(
+                gas(),
+                targetContract,
+                add(calldataPayload, 0x20),
+                mload(calldataPayload),
+                0,
+                0
+            )
 
-    function setLength(bytes memory buffer, uint256 length) internal pure {
-        assembly {
-            mstore(buffer, length)
+            mstore(0x00, success)
+            mstore(0x20, returndatasize())
+            returndatacopy(0x40, 0, returndatasize())
+            revert(0, add(returndatasize(), 0x40))
         }
     }
 }
