78e907b1a50d5c482bab1544d678e45faf6743bc
Nicholas Rodrigues Lordello <nicholas.lordello@gnosis.pm>
2021-03-29 09:22:22 +0200

    Port unit tests in separate commit

--- test/storage_accessible.ts
+++ test/storage_accessible.ts
@@ -1,122 +1,137 @@
-const StorageAccessibleWrapper = artifacts.require('StorageAccessibleWrapper')
-const ExternalStorageReader = artifacts.require('ExternalStorageReader')
+import { expect } from "chai";
+import { BigNumber, BigNumberish, ContractFactory } from "ethers";
+import { ethers } from "hardhat";
 
-const truffleAssert = require('truffle-assertions')
+describe("StorageAccessible", () => {
+  const fromHex = (data: string, start?: number, end?: number) =>
+    BigNumber.from(ethers.utils.hexDataSlice(data, start || 0, end));
+  const keccak = (numbers: BigNumberish[]) =>
+    ethers.utils.solidityKeccak256(
+      numbers.map(() => "uint256"),
+      numbers,
+    );
 
-contract('StorageAccessible', () => {
-  const fromHex = string => parseInt(string, 16)
-  const keccak = numbers => web3.utils.soliditySha3(
-    ...numbers.map(v => ({ type: 'uint256', value: v }))
-  )
+  let StorageAccessibleWrapper: ContractFactory;
+  let ExternalStorageReader: ContractFactory;
 
-  describe('getStorageAt', async () => {
-    it('can read statically sized words', async () => {
-      const instance = await StorageAccessibleWrapper.new()
-      await instance.setFoo(42)
+  before(async () => {
+    StorageAccessibleWrapper = await ethers.getContractFactory("StorageAccessibleWrapper");
+    ExternalStorageReader = await ethers.getContractFactory("ExternalStorageReader");
+  });
 
-      assert.equal(42, await instance.getStorageAt(await instance.SLOT_FOO(), 1))
-    })
-    it('can read fields that are packed into single storage slot', async () => {
-      const instance = await StorageAccessibleWrapper.new()
-      await instance.setBar(7)
-      await instance.setBam(13)
+  describe("getStorageAt", async () => {
+    it("can read statically sized words", async () => {
+      const instance = await StorageAccessibleWrapper.deploy();
+      await instance.setFoo(42);
 
-      const data = await instance.getStorageAt(await instance.SLOT_BAR(), 1)
+      expect(await instance.getStorageAt(await instance.SLOT_FOO(), 1)).to.equal(
+        ethers.utils.solidityPack(["uint256"], [42]),
+      );
+    });
+    it("can read fields that are packed into single storage slot", async () => {
+      const instance = await StorageAccessibleWrapper.deploy();
+      await instance.setBar(7);
+      await instance.setBam(13);
 
-      assert.equal(7, fromHex(data.slice(34, 66)))
-      assert.equal(13, fromHex(data.slice(18, 34)))
-    })
-    it('can read arrays in one go', async () => {
-      const instance = await StorageAccessibleWrapper.new()
-      const slot = await instance.SLOT_BAZ()
-      await instance.setBaz([1, 2])
+      const data = await instance.getStorageAt(await instance.SLOT_BAR(), 1);
+      expect(data).to.equal(
+        ethers.utils.hexZeroPad(ethers.utils.solidityPack(["uint64", "uint128"], [13, 7]), 32),
+      );
+    });
+    it("can read arrays in one go", async () => {
+      const instance = await StorageAccessibleWrapper.deploy();
+      const slot = await instance.SLOT_BAZ();
+      await instance.setBaz([42, 1337]);
 
-      const length = await instance.getStorageAt(slot, 1)
-      assert.equal(fromHex(length), 2)
+      const length = await instance.getStorageAt(slot, 1);
+      expect(BigNumber.from(length)).to.equal(2);
 
-      const data = await instance.getStorageAt(keccak([slot]), length)
-      assert.equal(1, fromHex(data.slice(2, 66)))
-      assert.equal(2, fromHex(data.slice(66, 130)))
-    })
-    it('can read mappings', async () => {
-      const instance = await StorageAccessibleWrapper.new()
-      await instance.setQuxKeyValue(42, 69)
-      assert.equal(69, await instance.getStorageAt(keccak([42, await instance.SLOT_QUX()]), 1))
-    })
+      const data = await instance.getStorageAt(keccak([slot]), length);
+      expect(fromHex(data, 0, 32)).to.equal(42);
+      expect(fromHex(data, 32, 64)).to.equal(1337);
+    });
+    it("can read mappings", async () => {
+      const instance = await StorageAccessibleWrapper.deploy();
+      await instance.setQuxKeyValue(42, 69);
+      expect(
+        fromHex(await instance.getStorageAt(keccak([42, await instance.SLOT_QUX()]), 1)),
+      ).to.equal(69);
+    });
 
-    it('can read structs', async () => {
-      const instance = await StorageAccessibleWrapper.new()
-      await instance.setFoobar(19, 21)
+    it("can read structs", async () => {
+      const instance = await StorageAccessibleWrapper.deploy();
+      await instance.setFoobar(19, 21);
 
-      const packed = await instance.getStorageAt(await instance.SLOT_FOOBAR(), 10)
-      assert.equal(19, fromHex(packed.slice(2, 66)))
-      assert.equal(21, fromHex(packed.slice(66, 130)))
-    })
-  })
+      const packed = await instance.getStorageAt(await instance.SLOT_FOOBAR(), 10);
+      expect(fromHex(packed, 0, 32)).to.equal(19);
+      expect(fromHex(packed, 32, 64)).to.equal(21);
+    });
+  });
 
-  describe('simulateDelegatecall', async () => {
-    it('can invoke a function in the context of a previously deployed contract', async () => {
-      const instance = await StorageAccessibleWrapper.new()
-      await instance.setFoo(42)
+  describe("simulateDelegatecall", async () => {
+    it("can invoke a function in the context of a previously deployed contract", async () => {
+      const instance = await StorageAccessibleWrapper.deploy();
+      await instance.setFoo(42);
 
       // Deploy and use reader contract to access foo
-      const reader = await ExternalStorageReader.new()
-      const getFooCall = reader.contract.methods.getFoo().encodeABI()
-      const result = await instance.simulateDelegatecall.call(reader.address, getFooCall)
-      assert.equal(42, fromHex(result))
-    })
+      const reader = await ExternalStorageReader.deploy();
+      const getFooCall = reader.interface.encodeFunctionData("getFoo");
+      const result = await instance.callStatic.simulateDelegatecall(reader.address, getFooCall);
+      expect(fromHex(result)).to.equal(42);
+    });
 
-    it('can simulate a function with side effects (without executing)', async () => {
-      const instance = await StorageAccessibleWrapper.new()
-      await instance.setFoo(42)
+    it("can simulate a function with side effects (without executing)", async () => {
+      const instance = await StorageAccessibleWrapper.deploy();
+      await instance.setFoo(42);
 
       // Deploy and use reader contract to simulate setting foo
-      const reader = await ExternalStorageReader.new()
-      const replaceFooCall = reader.contract.methods.setAndGetFoo(69).encodeABI()
-      let result = await instance.simulateDelegatecall.call(reader.address, replaceFooCall)
-      assert.equal(69, fromHex(result))
+      const reader = await ExternalStorageReader.deploy();
+      const replaceFooCall = reader.interface.encodeFunctionData("setAndGetFoo", [69]);
+      let result = await instance.callStatic.simulateDelegatecall(reader.address, replaceFooCall);
+      expect(fromHex(result)).to.equal(69);
 
       // Make sure foo is not actually changed
-      const getFooCall = reader.contract.methods.getFoo().encodeABI()
-      result = await instance.simulateDelegatecall.call(reader.address, getFooCall)
-      assert.equal(42, fromHex(result))
-    })
+      const getFooCall = reader.interface.encodeFunctionData("getFoo");
+      result = await instance.callStatic.simulateDelegatecall(reader.address, getFooCall);
+      expect(fromHex(result)).to.equal(42);
+    });
 
-    it('can simulate a function that reverts', async () => {
-      const instance = await StorageAccessibleWrapper.new()
+    it("can simulate a function that reverts", async () => {
+      const instance = await StorageAccessibleWrapper.deploy();
 
-      const reader = await ExternalStorageReader.new()
-      const doRevertCall = reader.contract.methods.doRevert().encodeABI()
-      truffleAssert.reverts(instance.simulateDelegatecall.call(reader.address, doRevertCall))
-    })
+      const reader = await ExternalStorageReader.deploy();
+      const doRevertCall = reader.interface.encodeFunctionData("doRevert");
+      await expect(instance.callStatic.simulateDelegatecall(reader.address, doRevertCall)).to.be
+        .reverted;
+    });
 
-    it('allows detection of reverts when invoked from other smart contract', async () => {
-      const instance = await StorageAccessibleWrapper.new()
+    it("allows detection of reverts when invoked from other smart contract", async () => {
+      const instance = await StorageAccessibleWrapper.deploy();
 
-      const reader = await ExternalStorageReader.new()
-      truffleAssert.reverts(reader.invokeDoRevertViaStorageAccessible(instance.address))
-    })
-  })
+      const reader = await ExternalStorageReader.deploy();
+      await expect(reader.invokeDoRevertViaStorageAccessible(instance.address)).to.be.reverted;
+    });
+  });
 
-  describe('simulateStaticDelegatecall', () => {
-    it('can be called from a static context', async () => {
-      const instance = await StorageAccessibleWrapper.new()
-      await instance.setFoo(42)
+  describe("simulateStaticDelegatecall", () => {
+    it("can be called from a static context", async () => {
+      const instance = await StorageAccessibleWrapper.deploy();
+      await instance.setFoo(42);
 
-      const reader = await ExternalStorageReader.new()
-      const getFooCall = reader.contract.methods.getFoo().encodeABI()
+      const reader = await ExternalStorageReader.deploy();
+      const getFooCall = reader.interface.encodeFunctionData("getFoo");
       // invokeStaticDelegatecall is marked as view
-      const result = await reader.invokeStaticDelegatecall(instance.address, getFooCall)
-      assert.equal(42, result)
-    })
+      const result = await reader.invokeStaticDelegatecall(instance.address, getFooCall);
+      expect(result).to.equal(42);
+    });
 
-    it('cannot simulate state changes', async () => {
-      const instance = await StorageAccessibleWrapper.new()
-      await instance.setFoo(42)
+    it("cannot simulate state changes", async () => {
+      const instance = await StorageAccessibleWrapper.deploy();
+      await instance.setFoo(42);
 
-      const reader = await ExternalStorageReader.new()
-      const replaceFooCall = reader.contract.methods.setAndGetFoo(69).encodeABI()
-      truffleAssert.reverts(reader.invokeStaticDelegatecall(reader.address, replaceFooCall))
-    })
-  })
-})
+      const reader = await ExternalStorageReader.deploy();
+      const replaceFooCall = reader.interface.encodeFunctionData("setAndGetFoo", [69]);
+      await expect(reader.invokeStaticDelegatecall(reader.address, replaceFooCall)).to.be.reverted;
+    });
+  });
+});
